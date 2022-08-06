ce437b75234a958a59f4f1cb1f86bfe473c6d599
jiaqi07 <wangjiaqi07@inspur.com>
2022-08-06 08:58:46 +0800

    remove unicode from code
    Change-Id: I23078d93d768deee0e8b93cbe254aea104e21f33

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-copyright = u'2016, OpenStack Foundation'
+copyright = '2016, OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
@@ -70,8 +70,8 @@ htmlhelp_basename = 'ironic-tempest-plugindoc'
 latex_documents = [
     ('index',
      'doc-ironic-tempest-plugin.tex',
-     u'Ironic Tempest Plugin Documentation',
-     u'OpenStack Foundation', 'manual'),
+     'Ironic Tempest Plugin Documentation',
+     'OpenStack Foundation', 'manual'),
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
