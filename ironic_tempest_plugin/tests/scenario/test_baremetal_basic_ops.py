26bf3b501443ad9592446aac702afb4e6ca0b8b6
Julia Kreger <juliaashleykreger@gmail.com>
2025-01-27 06:36:34 -0800

    Remove reboot from basic baremetal ops
    While investigating the 4k job, I noticed the test sometimes has trouble
    SSHing back in to the host post-reboot due to no fault of the test.
    
    In other tests, it looks like we manage to get SSH'ed in just as the
    deployed host is online.
    
    This is because cloud-init or other configuration changes have been
    applied in the OS, which is outside the act of "deploying" the node.
    
    In other words, we were spinning our basic job for several hundred
    seconds in some cases on just waiting for the OS to reboot, and then
    trying to verify the OS behaves the same as before we rebooted.
    
    Ultimately, to conserve resources *and* reduce failure risk by trying
    to test OS behavior, removing the reboot call and post-reboot re-ssh/ping
    check behavior.
    
    Change-Id: Ie08335e0e10f51f782a27270c62e33aa84a8c568

--- ironic_tempest_plugin/tests/scenario/test_baremetal_basic_ops.py
+++ ironic_tempest_plugin/tests/scenario/test_baremetal_basic_ops.py
@@ -237,20 +237,6 @@ class BaremetalBasicOps(baremetal_manager.BaremetalScenarioTest):
             self.rescue_instance(self.instance, self.node, ip_address)
             self.unrescue_instance(self.instance, self.node, ip_address)
 
-        # Reboot node
-        self.reboot_node(self.instance)
-
-        # Ensure we have some sort of connectivity
-        # Attempt to ping, if all else fails fall back to an ssh connection
-        # which worked previously.
-        pinging = self.ping_ip_address(ip_address)
-        if not pinging:
-            self.get_remote_client(ip_address, server=self.instance)
-        else:
-            # If we're here, this is successful. If ssh fails above,
-            # the job will ultimately fail.
-            self.assertTrue(pinging)
-
         self.terminate_instance(self.instance)
 
     @decorators.idempotent_id('549173a5-38ec-42bb-b0e2-c8b9f4a08943')
