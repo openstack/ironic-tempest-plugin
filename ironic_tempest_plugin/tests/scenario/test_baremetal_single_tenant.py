9ea0e36d321fa95fd58b390c50786e739fee7cb6
Julia Kreger <juliaashleykreger@gmail.com>
2025-05-16 14:01:00 -0700

    Remove available nodes configuration
    Tempest had a knob for the number of available nodes, but it
    addes un-necessary complexity. Removing.
    
    Change-Id: I02ade314ae8f57cb61b2d0b05a6fe60482866c6d

--- ironic_tempest_plugin/tests/scenario/test_baremetal_single_tenant.py
+++ ironic_tempest_plugin/tests/scenario/test_baremetal_single_tenant.py
@@ -44,11 +44,6 @@ class BaremetalSingleTenant(baremetal_manager.BaremetalScenarioTest,
         if not CONF.baremetal.use_provision_network:
             msg = 'Ironic/Neutron tenant isolation is not configured.'
             raise cls.skipException(msg)
-        if (CONF.baremetal.available_nodes is not None
-                and CONF.baremetal.available_nodes < 2):
-            msg = ('Not enough baremetal nodes, %d configured, test requires '
-                   'a minimum of 2') % CONF.baremetal.available_nodes
-            raise cls.skipException(msg)
 
     def create_tenant_network(self, clients, tenant_cidr):
         network = self.create_network(
